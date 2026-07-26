847c7f20eeaaf788c9fbf1c1c565441e5aa6520d
Harshini Umesh <harshinividya127@gmail.com>
2026-07-26 14:43:25 +0530

    Add files via upload

---
+++ train_demo.py
@@ -0,0 +1,31 @@
+{
+  "feature_names": [
+    "age",
+    "sex",
+    "cp",
+    "trestbps",
+    "chol",
+    "fbs",
+    "restecg",
+    "thalach",
+    "exang",
+    "oldpeak",
+    "slope",
+    "ca",
+    "thal"
+  ],
+  "n_features": 13,
+  "accuracy": 0.8525,
+  "roc_auc": 0.9481,
+  "confusion_matrix": [
+    [
+      30,
+      3
+    ],
+    [
+      6,
+      22
+    ]
+  ],
+  "model_type": "sklearn_mlp_demo"
+}
